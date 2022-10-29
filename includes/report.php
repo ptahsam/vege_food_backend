7c04e8269502fad98fc8e691e3db3cfffb572b5c
Peter Samuel <ptahsamuel@gmail.com>
2022-10-29 12:02:25 +0300

    first commit

--- includes/report.php
+++ includes/report.php
@@ -21,15 +21,22 @@ class Report
 		$pdf->SetTitle('VegeFood Orders');
 		$pdf->SetFont('Times');
 		$pdf->SetTextColor(117,117,60);
-		$pdf->cell(190, 50, $pdf->Image('../images/icons/logo_only.png', $pdf->GetX(), $pdf->GetY(), 40), 0, 1, 'C', false);
-		$pdf->cell(190, 5, '', 0, 1);
+		$pdf->cell(190, 20, $pdf->Image('../images/icons/logo_only.png', $pdf->GetX(), $pdf->GetY(), 95), 0, 1, 'C', false);
+		//$pdf->cell(190, 5, '', 0, 1);
 		$pdf->SetFont('Times', 'B', '20');
-		$pdf->SetTextColor(1,1,1);
-		$pdf->cell(190, 40, "VegeFood", 0, 1, 'C', false);
-		$pdf->cell(190, 5, '', 0, 1);
-		$pdf->SetFont('Times', 'B', '14');
+		$pdf->SetTextColor(45, 206, 137);
+		$pdf->cell(190, 12, "VegeFood", 0, 1, 'C', false);
+		//$pdf->cell(190, 5, '', 0, 1);
+		$pdf->SetFont('Times', 'B', '10');
 		$pdf->SetTextColor(9,9,9);
 		$pdf->cell(190, 12, date("F d, Y H:i A", $timestamp), 0, 1, "R", false);
+		$pdf->cell(190, 10, '', 0, 1);
+		$pdf->SetFont('Times', 'B', '13');
+		$pdf->SetTextColor(45, 206, 137);
+		$pdf->cell(10,12,'','L',true);
+		$pdf->cell(180,12,'Order Details',0,1,'L',true);
+
+
 		$pdf->PageNo();
 		$pdf->Output("../reports/order".$order_refno.$timestamp.".pdf","F");
 		return $timestamp;
